2b56df27077d6af44c8f91c8ba556ce43b045c9e
Ziltener, Stefanie <stefanie.ziltener@zuehlke.com>
2021-09-27 15:25:23 +0200

    Create REST endpoint to get one result for a search term

---
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/presentation/UserResource.java
@@ -0,0 +1,43 @@
+package ch.zuehlke.fullstack.hackathon.presentation;
+
+import ch.zuehlke.fullstack.hackathon.domain.UserInfo;
+import ch.zuehlke.fullstack.hackathon.integration.EmployeeClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserResource {
+
+    private final EmployeeClient employeeClient;
+
+    @Autowired
+    public UserResource(EmployeeClient employeeClient) {
+        this.employeeClient = employeeClient;
+    }
+    
+    @GetMapping("/search")
+    public ResponseEntity<UserInfo> getExample(@RequestParam String term) {
+        UserInfo result;
+        try {
+            List<UserInfo> response = this.employeeClient.getEmployees(term);
+            if(response != null && response.size() > 0) {
+                result = response.get(0);
+                return new ResponseEntity<>(result, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+    }
+
+}
