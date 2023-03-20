9c1751698c7fe199acba3108c43ea43cbca5e6b8
anko <andres.konrad@hotmail.com>
2023-03-20 08:22:57 +0100

    Wrote test for StompClient

---
+++ bot/src/test/java/ch/zuehlke/challenge/bot/client/StompClientTest.java
@@ -0,0 +1,33 @@
+package ch.zuehlke.challenge.bot.client;
+
+import ch.zuehlke.challenge.bot.service.GameService;
+import ch.zuehlke.challenge.bot.util.ApplicationProperties;
+import ch.zuehlke.common.GameUpdate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class StompClientTest {
+
+    private StompClient stompClient;
+
+    private ApplicationProperties applicationPropertiesMock;
+
+    private GameService gameServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        applicationPropertiesMock = mock(ApplicationProperties.class);
+        gameServiceMock = mock(GameService.class);
+        stompClient = new StompClient(applicationPropertiesMock, gameServiceMock);
+    }
+
+    @Test
+    void handleFrame_updatesGame_successfully() {
+        GameUpdate emptyGameUpdate = new GameUpdate(null);
+        stompClient.handleFrame(null, emptyGameUpdate);
+
+        verify(gameServiceMock, times(1)).onGameUpdate(emptyGameUpdate);
+    }
+}
