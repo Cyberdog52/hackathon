8035f6b23ef156c3c0e8f49e9ec3fc47ef36af13
Nick Brönnimann <nick.broennimann@zuehlke.com>
2023-03-27 17:03:52 +0200

    persistence: remove unused example game code

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/Lobby.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/Lobby.java
@@ -1,5 +1,15 @@
 package ch.zuehlke.fullstack.hackathon.model;
 
 public record Lobby(Game game, int minPlayerCount, int maxPlayerCount) {
+  public boolean addPlayer(ThunderShipsPlayer player) {
+    if (game.getPlayers().size() < maxPlayerCount) {
+      return game.getPlayers().add(player);
+    }
+    return false;
+  }
+
+  public boolean canStartGame() {
+    return game.getPlayers().size() >= minPlayerCount;
+  }
 
 }
