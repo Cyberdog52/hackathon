da75127f93be9f8ceb2e7232b86159034eed4813
Marius Niklaus <marius.niklaus@zuehlke.com>
2023-03-28 11:44:00 +0200

    prettier game layout with moves

--- frontend/src/app/lobby/lobby.component.ts
+++ frontend/src/app/lobby/lobby.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from "@angular/core";
-import { LobbyService } from "../../services/lobby.service";
-import { Observable, repeat, retry } from "rxjs";
-import { GameDto } from "../../model/lobby";
+import { Component, OnInit } from '@angular/core';
+import { LobbyService } from '../../services/lobby.service';
+import { Observable, repeat, retry } from 'rxjs';
+import { GameDto } from '../../model/lobby';
 
 @Component({
-  selector: "app-lobby",
-  templateUrl: "./lobby.component.html",
-  styleUrls: ["./lobby.component.scss"],
+  selector: 'app-lobby',
+  templateUrl: './lobby.component.html',
+  styleUrls: ['./lobby.component.scss'],
 })
 export class LobbyComponent implements OnInit {
   games$: Observable<GameDto[]> | undefined;
@@ -28,12 +28,12 @@ export class LobbyComponent implements OnInit {
     this.lobbyService.createGame().subscribe({
       next: (gameId) => {
         // Improve: Do something with the gameId
-        console.log("Created new game with id: ", gameId);
+        console.log('Created new game with id: ', gameId);
       },
       error: (error) => {
         // Improve: Do something with the error
         console.log(
-          "Something went wrong during creation of new game: ",
+          'Something went wrong during creation of new game: ',
           error
         );
       },
