08ff5ae7d715f9754dd851210293d5f5284786ba
lonelyPorterCH <fabio.stuber@gmail.com>
2023-03-28 11:15:43 +0200

    Add GroupTest by Cyberdog52

---
+++ backend/src/test/java/ch/zuehlke/fullstack/hackathon/model/GroupTest.java
@@ -0,0 +1,65 @@
+package ch.zuehlke.fullstack.hackathon.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static ch.zuehlke.fullstack.hackathon.model.CardValue.*;
+import static ch.zuehlke.fullstack.hackathon.model.Suit.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GroupTest {
+
+    @Test
+    void constructGroup_withSingleCard_intoASingleGroup() {
+        var card = new Card(CLUBS, JACK);
+        var group = new Group(Set.of(card), GroupType.SINGLE);
+
+        assertEquals(GroupType.SINGLE, group.type());
+    }
+
+    @Test
+    void constructGroup_withTwoCards_intoASingleGroup() {
+        var cards = Set.of(new Card(CLUBS, JACK), new Card(HEART, JACK));
+        var group = new Group(cards, GroupType.SINGLE);
+
+        assertEquals(GroupType.SINGLE, group.type());
+    }
+
+    @Test
+    void constructGroup_withThreeDifferentCards_intoASingleGroup() {
+        var cards = Set.of(new Card(CLUBS, SIX), new Card(HEART, JACK), new Card(SPADE, ACE));
+        var group = new Group(cards, GroupType.SINGLE);
+
+        assertEquals(GroupType.SINGLE, group.type());
+    }
+
+    @Test
+    void constructGroup_withThreeNeighboringCards_intoASequence() {
+        var cards = Set.of(new Card(CLUBS, JACK), new Card(CLUBS, QUEEN), new Card(CLUBS, KING));
+
+        var group = new Group(cards);
+
+        assertEquals(GroupType.SEQUENCE, group.type());
+    }
+
+    @Test
+    void constructGroup_threeMatchingCards_intoATriplet() {
+        var cards = Set.of(new Card(CLUBS, JACK), new Card(HEART, JACK), new Card(SPADE, JACK));
+
+        var group = new Group(cards);
+
+        assertEquals(GroupType.TRIPLET, group.type());
+    }
+
+    @Test
+    void constructGroup_fourMatchingCards_intoAQuartet() {
+        var cards = Set.of(new Card(CLUBS, JACK), new Card(HEART, JACK), new Card(SPADE, JACK), new Card(DIAMOND, JACK));
+
+        var group = new Group(cards);
+
+        assertEquals(GroupType.QUARTET, group.type());
+    }
+
+
+}
