c47448e98ad99af57fdd2b260122799eacf02c14
lonelyPorterCH <fabio.stuber@gmail.com>
2023-03-28 14:23:03 +0200

    Add build folder to git ignore

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/RuleEngine.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/service/RuleEngine.java
@@ -10,21 +10,23 @@ public class RuleEngine {
     public static final int NUMBER_OF_HANDCARDS = 7;
 
     public static int calculateScore(Set<Card> cards) {
-        if (cards.size() != NUMBER_OF_HANDCARDS) {
-            throw new IllegalArgumentException("Number of cards must be " + NUMBER_OF_HANDCARDS);
-        }
+        validateCardValidity(cards);
 
-         return Grouping.group(cards).getNumberOfPoints();
+        return Grouping.group(cards).getNumberOfPoints();
     }
 
     /**
      * Calculates of the hand is winning and returns the score
      */
     public static boolean isWinning(Set<Card> cards) {
-if (cards.size() != NUMBER_OF_HANDCARDS) {
-            throw new IllegalArgumentException("Number of cards must be " + NUMBER_OF_HANDCARDS);
-        }
+        validateCardValidity(cards);
 
         return Grouping.group(cards).isWinningHand();
+    }
+
+    private static void validateCardValidity(Set<Card> cards) {
+        if (cards.size() != NUMBER_OF_HANDCARDS) {
+            throw new IllegalArgumentException("Number of cards must be " + NUMBER_OF_HANDCARDS);
+        }
     }
 }
